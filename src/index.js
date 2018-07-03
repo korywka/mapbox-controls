37e8c1b8d3e9461dee3a276d09238b44ac79ac26
bravecow <neporozhniy@gmail.com>
2018-07-03 16:22:26 +0300

    ruler export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { default as CompassControl } from './mapbox-gl-compass';
+export { default as RulerControl } from './mapbox-gl-ruler';
 export { default as LocalizationControl } from './mapbox-gl-localization';
 export { default as StylesControl } from './mapbox-gl-styles';
 export { default as ZoomControl } from './mapbox-gl-zoom';
